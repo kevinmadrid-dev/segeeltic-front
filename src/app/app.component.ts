9bbd9a42d8046e158b3f4541e444c6f501a0703b
kevinmadrid-dev <kevin_madrid10@hotmail.es>
2025-08-28 12:31:34 -0500

    fix: fix routes page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core"
 
-import { HomePageComponent } from "./features/home-page/home-page.component"
+import { RouterOutlet } from "@angular/router"
 
 @Component({
   selector: "app-root",
-  imports: [HomePageComponent],
+  imports: [RouterOutlet],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css"
 })
